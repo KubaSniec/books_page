a499d3663c6308b410b69625544153d08e5d0ecc
Kubsonix <kuba.sniec@gmail.com>
2016-02-15 15:09:55 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create!(author: "Mario Puzo",
+             title: "Ojciec Chrzestny",
+             description: "Don Vito Corleone jest Ojcem Chrzestnym jednej z sześciu nowojorskich rodzin mafijnych. Tyran i szantażysta (słynne powiedzenie „mam dla Ciebie propozycję nie do odrzucenia”), a zarazem człowiek honoru, sprawuje rządy żelazną ręką. Jego decyzje mają charakter ostateczny.",
+             ISBN_number: 9788378858270,
+             page_count: 480,
+             year: 2013,
+             publisher: "Albatros",
+             cover: File.new("app/assets/images/book1.jpg"));
+
+Book.create!(author: "Gillian Flynn",
+            title: "Ostre przedmioty",
+            description: "Camille Preaker po krótkim pobycie w klinice psychiatrycznej wraca do pracy reporterki w „Daily Post” i otrzymuje zlecenie na artykuł o tajemniczych zabójstwach dwóch małych dziewczynek w jej rodzinnym miasteczku. Z niechęcią wraca do wspaniałej wiktoriańskiej rezydencji, w której się wychowała, gdzie zaczyna ją prześladować wspomnienie tragedii z dzieciństwa.",
+            ISBN_number: 9788377789438,
+            page_count: 358,
+            year: 2015,
+            publisher: "Burda Książki",
+            cover: File.new("app/assets/images/book2.jpg"));
+
+Book.create!(author: "Franz Kafka",
+            title: "Proces",
+            description: "Bruno Schulz był artystą totalnym. Nie tylko malował i rysował, nie tylko pisał własne opowieści, ale również zajmował się tłumaczeniem. I to właśnie jemu zawdzięczamy genialny przekład Procesu Franza Kafki. Proces w tłumaczeniu Schulza ilustrowany jego grafikami.",
+            ISBN_number: 9788377792070,
+            page_count: 448,
+            year: 2014,
+            publisher: "Wydawnictwo MG",
+            cover: File.new("app/assets/images/book3.jpg"));
+
+Book.create!(author: "Nicholas Searle",
+            title: "Dobry kłamca",
+            description: "Czasem łatwiej uwierzyć w kłamstwo niż w prawdę… Jedna z najbardziej oczekiwanych książek 2016 roku – thriller psychologiczny, w którym nikt nie jest tym, za kogo się podaje! Intryga godna najlepszych powieści szpiegowskich Johna le Carre i powieści Utalentowany pan Ripley Patricii Highsmith: historia wytrawnego oszusta, który całe życie buduje na kłamstwie.",
+            ISBN_number: 9788328021235,
+            page_count: 512,
+            year: 2016,
+            publisher: "W.A.B",
+            cover: File.new("app/assets/images/book4.jpg"));
