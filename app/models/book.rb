b5a48327664062054aecd787060b425c82ae45a5
Kubsonix <kuba.sniec@gmail.com>
2016-02-15 13:58:44 +0100

    Add validates to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ActiveRecord::Base
   has_attached_file :cover, styles: { medium: "300x400#" }
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :ISBN_number, presence: true, length: { in: 10..13 }, uniqueness: true
+  validates :year, presence: true, length: { minimum: 4}
+  validates :publisher, presence: true
 end
